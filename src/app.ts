a1042da3f884e974fb713525c6f097c5429295cd
sarker24 <sarker24@gmail.com>
2023-05-31 10:00:36 +0200

    setup eslint prettier and lint-staged
    setup eslint prettier and lint-staged second
    
    setup eslint prettier and lint-staged third
    
    setup eslint prettier and lint-staged fourth
    
    setup eslint prettier and lint-staged fifth

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,13 @@ const app = express()
 
 //perser
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
 app.use(cors)
-app.get('/', (req:Request, res:Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
-
 export default app
+
+// AM0421
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,20 @@
-import mongoose from "mongoose";
-import config  from "./config/index";
-import app from "./app";
+import mongoose from 'mongoose'
+import config from './config/index'
+import app from './app'
 
 async function bootstrap() {
-    try {
-        console.log(config.database_url)
-        await mongoose.connect(config.database_url as string);
+  try {
+    console.log(config.database_url)
+    await mongoose.connect(config.database_url as string)
 
-        console.log('database is connect successfully');
+    console.log('database is connect successfully')
 
-        app.listen(config.port, () => {
-            console.log(`Application listening on port ${config.port}`)
-          })
-        
-  
-    } catch (err) {
-        console.log('fail to connect databser', err);
-        
-    }
-   
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`)
+    })
+  } catch (err) {
+    console.log('fail to connect databser', err)
   }
+}
 
-  bootstrap()
+bootstrap()
