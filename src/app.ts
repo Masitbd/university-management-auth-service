fd5903698aba40359b62c1cbd4cc75068e505eb5
sarker24 <sarker24@gmail.com>
2023-06-01 17:57:18 +0200

    Improve lint setup

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,26 @@
-import express, { Request, Response } from 'express'
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-const app = express()
+import usersRouter from './app/modules/users/users.route'
 
-//perser
+const app: Application = express()
+
+app.use(cors())
+
+//parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use(cors)
-app.get('/', (req: Request, res: Response) => {
+// Application routes
+
+app.use('/api/v1/users/', usersRouter)
+
+app.get('/', async (req: Request, res: Response) => {
+  /* await usersServices.createUser({
+    id: '999',
+    password: '1234',
+    role: 'admin',
+  }) */
+
   res.send('Hello World!')
 })
 
--- src/config/index.ts
+++ src/config/index.ts
@@ -7,4 +7,5 @@ console.log(process.env.PORT)
 export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
+  default_user_pass: process.env.DEFAULT_USER_PASS,
 }
