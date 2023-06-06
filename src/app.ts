721da43107ff9aa5251f6d64495a59f22b612873
sarker24 <sarker24@gmail.com>
2023-06-06 11:29:09 +0200

    Done logger error formate

--- src/app.ts
+++ src/app.ts
@@ -27,8 +27,13 @@ app.use('/api/v1/users/', UserRoutes)
 
   res.send('Hello World!')
  */
-//throw new ApiError(400, 'ora bana')
+// throw new ApiError(400, 'ora bana')
 // global error handling
+// })
+
+//Testing
+// app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+// throw new Error('Testing Error logger')
 // })
 
 app.use(globalErrorHandler)
